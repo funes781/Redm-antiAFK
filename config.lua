1cadb09655dda2fccd921efc62cf14f0b1287c88
funes781 <121256208+funes781@users.noreply.github.com>
2023-10-29 21:09:42 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.kickWarning = true -- notification warning
+Config.secondsUntilKick = 60 -- How long they can be AFK before they get kicked in seconds
+Config.KickSoonMsg = "Niedługo zostaniesz wyrzucony za brak aktywności aby temu zapobiec użyj ^1/play"
+Config.KickMsg = "Zostałeś wyrzucony z serwera za bycie AFK"
